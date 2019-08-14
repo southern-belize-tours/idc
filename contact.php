a86b514ae0bcccde39b694d0cc08f0c20796c0b2
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2019-08-13 23:17:00 -0700

    fiddling with php

---
+++ contact.php
@@ -0,0 +1,20 @@
+
+<?php> 
+
+
+if($_POST["submit"]) {
+    $recipient="ianfeekes@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+
+?> 
