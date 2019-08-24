2df963bb1ee69e6be62711391e401313c051b5ad
Ian Feekes <ianfeekes@Ians-MacBook-Pro.local>
2019-08-23 18:45:16 -0700

    attempting php

--- contact.php
+++ contact.php
@@ -1,21 +1,22 @@
 
 <?php
 
+if(isset($_POST['submit']))
+{
+    $name = $_POST['sender'];
+    $email = $_POST['senderEmail'];
+    $subject = "temp"; 
 
-if($_POST["submit"]) {
-    $recipient="ianfeekes@gmail.com";
-    $subject="Form to email message";
-    /*$sender=$_POST["sender"];*/ 
-    $sender="ianfeekes"; 
-   /* $senderEmail=$_POST["senderEmail"];*/ 
-    $senderEmail="ianfeekes@gmail.com"; 
-    $message=$_POST["message"];
-    /*$mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-    */ 
-    $mailBody = "this is a temp body"; 
-    echo("This is some dumb script running"); 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+    $txt = $_POST['txt']; 
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    $message = "this is a temp message from".$name.".\n\n".$txt; 
+    $mailTo = "ifeekes@ucsc.edu"; 
+    $headers = "From: ".$email;
+
+
+    mail($email, $subject, $txt, $headers ); 
+    header("Location: contact.php?mailsend"); 
 }
+
+
 ?> 
