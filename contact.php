3ec282dcca6a7eb589df55f155b0fad658fbcb69
Ian Feekes <ianfeekes@Ians-MacBook-Pro.local>
2019-08-23 18:24:34 -0700

    trying to do some dumb php

--- contact.php
+++ contact.php
@@ -1,20 +1,21 @@
 
-<?php> 
+<?php
 
 
 if($_POST["submit"]) {
     $recipient="ianfeekes@gmail.com";
     $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
+    /*$sender=$_POST["sender"];*/ 
+    $sender="ianfeekes"; 
+   /* $senderEmail=$_POST["senderEmail"];*/ 
+    $senderEmail="ianfeekes@gmail.com"; 
     $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
+    /*$mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+    */ 
+    $mailBody = "this is a temp body"; 
+    echo("This is some dumb script running"); 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
     $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
-
-
 ?> 
