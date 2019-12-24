87cfbf9408446d2aee35034a391827b7f15fd2e6
Ian Feekes <ianfeekes@Ians-MacBook-Pro.local>
2019-12-23 20:02:51 -0800

    some basic fixes

--- assets/js/genericScript.js
+++ assets/js/genericScript.js
@@ -16,6 +16,7 @@
       var i;
       var slides = document.getElementsByClassName("mySlides");
       var dots = document.getElementsByClassName("dot");
+      if(slides.length==0 || dots.length==0)return; 
       if (n > slides.length) {slideIndex = 1}
       if (n < 1) {slideIndex = slides.length}
       for (i = 0; i < slides.length; i++) {
