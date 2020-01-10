589651d46e5f01ffdd717180750edcbf3a0a2a49
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-01-10 13:37:19 -0800

    uploading nimLi Square images, some script to auto load based on media size

---
+++ assets/js/homeScript.js
@@ -0,0 +1,44 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+x = w.innerWidth || e.clientWidth || g.clientWidth,
+y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+function setSizedSources(names){
+  let mainDiv = document.getElementById("gridImages");
+  console.log(mainDiv);
+  let imageElements = mainDiv.getElementsByClassName("grid-image");
+  let imageSrcEndString = "";
+  if(x>=768)imageSrcEndString="300";
+  else if(x>=680)imageSrcEndString="200";
+  else if(x>=525)imageSrcEndString="240";
+  else if(x>=425)imageSrcEndString="160";
+  else if(x>=375)imageSrcEndString="150";
+  else if(x>=320)imageSrcEndString="130";
+  else imageSrcEndString="200";
+  for(let i=0;i<imageElements.length;++i)
+  {
+    console.log("images/squares/"+names[i]+imageSrcEndString);
+    imageElements[i].src="images/square/"+names[i]+imageSrcEndString;
+
+  }
+
+}
+
+window.onload=function(){
+  if (x<568)
+  {
+
+  }
+  else
+  {
+
+  }
+
+  //This image names list will expand as we add more tour content
+  let imageNames = ["nimLiSquare","lubaantunSquare","spiceFarmSquare","southStaanSquare",
+    "waterfallsSquare","inlandBlueHoleSquare","blueHoleSquare","southSnorkelSquare","southFishSquare"];
+  setSizedSources(imageNames);
+  console.log("fuck");
+};
