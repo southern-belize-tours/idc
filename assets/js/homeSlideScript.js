f27456e9dcc308f6eae7c656b563d9b2e94d6219
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-01-15 07:23:33 -0800

    fixed bug I created with the slide script

--- assets/js/homeSlideScript.js
+++ assets/js/homeSlideScript.js
@@ -5,6 +5,25 @@ g = d.getElementsByTagName('body')[0],
 x = w.innerWidth || e.clientWidth || g.clientWidth,
 y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
+function setSizedSources(names){
+  let mainDiv = document.getElementById("gridImages");
+  console.log(mainDiv);
+  let imageElements = mainDiv.getElementsByClassName("grid-image");
+  let imageSrcEndString = "";
+  if(x>=768)imageSrcEndString="300";
+  else if(x>=680)imageSrcEndString="200";
+  else if(x>=525)imageSrcEndString="240";
+  else if(x>=425)imageSrcEndString="160";
+  else if(x>=375)imageSrcEndString="150";
+  else if(x>=320)imageSrcEndString="130";
+  else imageSrcEndString="200";
+  imageSrcEndString = imageSrcEndString+".jpg";
+  for(let i=0;i<imageElements.length;++i)
+  {
+    imageElements[i].src="images/square/"+names[i]+"/"+names[i]+imageSrcEndString;
+  }
+}
+
 function setSlideImages(){
   x=w.innerWidth || e.clientWidth || g.clientWidth;
   let t = document.getElementById("turtle");
@@ -13,6 +32,10 @@ function setSlideImages(){
 }
 
 window.onload=function(){
+  //This image names list will expand as we add more tour content
+  let imageNames = ["nimLiSquare","lubaantunSquare","spiceFarmSquare","southStaanSquare",
+    "waterfallsSquare","inlandBlueHoleSquare","blueHoleSquare","southSnorkelSquare","southFishSquare"];
+  setSizedSources(imageNames);
   setSlideImages();
 };
 
