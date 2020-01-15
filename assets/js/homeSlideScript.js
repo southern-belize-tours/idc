845ac993a2f7bb3a4bfc5fe6c8a14d80c6ba6a21
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-01-15 07:01:23 -0800

    turtle fixes

---
+++ assets/js/homeSlideScript.js
@@ -0,0 +1,16 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+x = w.innerWidth || e.clientWidth || g.clientWidth,
+y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+function setSlideImages(){
+  let t = document.getElementById("turtle");
+  if(x>370)t.src="images/home/turtle/turtle1024.jpeg";
+  else t.src="images/home/turtle/turtle600.jpeg";
+}
+
+window.onload=function(){
+  setSlideImages();
+};
