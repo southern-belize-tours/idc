5486a5661f52acbd69e766e436dc2392e67cdde5
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-02-01 23:12:17 -0800

    fuck

--- mail.php
+++ mail.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 //$message = $_POST['message'];
@@ -8,6 +13,6 @@ $recipient = "ianfeekes@gmail.com";
 $subject = "contact form";
 $mailheader = "From: $email \r\n";
 //mail($recipient, $subject, $formcontent, $mailheader) or die("Error: failed to send.");
-mail('ianfeekes@gmail.com','subject',$message) or die("fuck");
+mail('ianfeekes@gmail.com','subject',$message); //or die("fuck");
 echo "Thanks. Email sent";
 ?>
