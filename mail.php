249754503e25616230204ec6554324bd704aade4
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-02-01 23:14:43 -0800

    please work

--- mail.php
+++ mail.php
@@ -4,14 +4,14 @@ ini_set('display_errors', 'On');
 set_error_handler("var_dump");
 
 
-$name = $_POST['name'];
-$email = $_POST['email'];
+//$name = $_POST['name'];
+//$email = $_POST['email'];
 //$message = $_POST['message'];
 $message = 'hello';
-$formcontent="From: $name \n Message: $message";
-$recipient = "ianfeekes@gmail.com";
-$subject = "contact form";
-$mailheader = "From: $email \r\n";
+//$formcontent="From: $name \n Message: $message";
+//$recipient = "ianfeekes@gmail.com";
+//$subject = "contact form";
+//$mailheader = "From: $email \r\n";
 //mail($recipient, $subject, $formcontent, $mailheader) or die("Error: failed to send.");
 mail('ianfeekes@gmail.com','subject',$message); //or die("fuck");
 echo "Thanks. Email sent";
