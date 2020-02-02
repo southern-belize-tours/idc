ac17c84aa7750d1126bca6f001002a6ac717ba65
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-02-01 23:01:50 -0800

    messing with contact php

--- mail.php
+++ mail.php
@@ -1,10 +1,12 @@
-<?php $name = $_POST['name'];
+<?php
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "ianfeekes@gmail.com";
-$subject = "contact form"; 
+$subject = "contact form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error: failed to send.");
+//mail($recipient, $subject, $formcontent, $mailheader) or die("Error: failed to send.");
+mail("ianfeekes@gmail.com","hi","message","headers") or die("fuck"); 
 echo "Thanks. Email sent";
 ?>
