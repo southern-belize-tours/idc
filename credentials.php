cd730b1d576470ea65ef38a1ead4986ff5adba40
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-02-07 15:30:43 -0800

    echo debugging in credentials.php, switching action form files in contact.html

--- credentials.php
+++ credentials.php
@@ -2,17 +2,17 @@
     // You need to install the sendgrid client library so run:
     // composer require sendgrid/sendgrid
     require '/vendor/autoload.php';
-
+    echo "fuck"; 
     // contains a variable called: $API_KEY that is the API Key.
     // You need this API_KEY created on the Sendgrid website.
     //include_once('./credentials.php');
     $API_KEY='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';
 
-    $FROM_EMAIL = 'YOUR_EMAIL';
+    $FROM_EMAIL = 'ianfeekes@gmail.com';
     // they dont like when it comes from @gmail, prefers business
     // emails
 
-    $TO_EMAIL = 'THE_PERSON_YOUR_WANT_TO_CONTACT';
+    $TO_EMAIL = 'ifeekes@ucsc.edu';
     // Try to be nice. Take a look at the anti spam laws. In most
     // cases, you must have an unsubscribe. You also cannot be
     // misleading.
