9197ce9c98d83d90509fc19c29a36aace967b232
Ian Feekes <ianfeekes@Ians-MBP.attlocal.net>
2020-02-07 16:03:53 -0800

    another attempt

--- credentials.php
+++ credentials.php
@@ -1,35 +1,28 @@
 <?php
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
 
-$request_body = json_decode('{
-"personalizations": [
-  {
-    "to": [
-      {
-        "email": "ianfeekes@gmail.com"
-      }
-    ],
-    "subject": "Hello World from the SendGrid PHP Library!"
-  }
-],
-"from": {
-  "email": "ifeekes@ucsc.edu"
-},
-"content": [
-  {
-    "type": "text/plain",
-    "value": "Hello, Email!"
-  }
-]
-}');
-
-$apiKey = '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';
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($request_body);
-echo $response->statusCode();
-echo "<br>";
-echo $response->body();
-echo "<br>";
-echo $response->headers();
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("ianfeekes@gmail.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("ifeekes@ucs.cedu", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
 ?>
