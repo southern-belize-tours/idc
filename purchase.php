dedc20252d934b530797c29f0fa60f3833dd3f1f
Ian Feekes <ianfeekes@ians-mbp.home>
2020-03-01 13:56:14 -0800

    attempt at storing tours as 2D array

--- purchase.php
+++ purchase.php
@@ -13,19 +13,35 @@
   $mail->SetFrom('no-reply@southern-belize-tours.com');
   $mail->Subject = 'Booking Confirmed!';
 
-  $tourOptions = array('nimLi','spiceFarm','jungleTube','waterfallSightseeing','lubaantun',
-        'inlandBlueHole','blueHoleFlyover','southwaterSnorkelHalf','southwaterSnorkelFull',
-        'southWaterFishing','atmCave','caveTubing','twinCities','ziplining','zoo');
+  $tourOptions = array(
+    array('nimLi','Nim Li Punit Mayan ruins'),
+    array('spiceFarm','Spice Farm tropical botanical gardens'),
+    array('jungleTube','South Staan Creek jungle river tubing'),
+    array('waterfallSightseeing','Waterfall sightseeing'),
+    array('lubaantun','Lubaantun Mayan Ruins'),
+    array('inlandBlueHole','Inland Blue Hole'),
+    array('blueHoleFlyover','Great Blue Hole flyover'),
+    array('southwaterSnorkelHalf','Southwater Caye snorkeling (full day)'),
+    array('southwaterSnorkelFull','Southwater Caye snorkeling (half day)'),
+    array('southWaterFishing','Southwater Caye fishing'),
+    array('atmCave','ATM Cave adventure'),
+    array('caveTubing','Cave Tubing'),
+    array('twinCities','Twin Cities Mayan ruins'),
+    array('ziplining','Jungle Ziplining'),
+    array('zoo','Belize Zoo'
+  );
+
   $checkedTours = "";
   foreach($tourOptions as $tour){
-    if(!empty($_POST[$tour]))
+    if(!empty($_POST[$tour[0]]))
     {
-      $checkedTours.=$tour . '<br';
+      $checkedTours.=$tour[1] . '<br';
     }
   }
 
   $messageBody = 'Dear ' .$_POST['first_name'].' ' .$_POST['last_name'] .',<br>'
-              . 'Congrats on your booking on '.$_POST['month'] .'/'.$_POST['day'].'/20' .$_POST['year'] . '! We are very excited to show you the wonders of Belize! <br>';
+              . 'Congrats on your booking on '.$_POST['month'] .'/'.$_POST['day'].'/20' .$_POST['year'] . '! We are very excited to show you the wonders of Belize! <br>'
+              .$checkedTours;
 
   $mail->Body = $messageBody;
   $mail->isHTML();
