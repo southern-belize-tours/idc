2b2af681ad7f2b6dbf89e38d9a9f79f27ec2db1d
Ian Feekes <ianfeekes@ians-mbp.home>
2020-03-01 14:43:40 -0800

    fixed string concatenation error

--- purchase.php
+++ purchase.php
@@ -46,7 +46,7 @@
   $messageBody = '<div style="font-size:18;color: #222222;">Dear ' .$_POST['first_name'].' ' .$_POST['last_name'] .',<br>'
               . 'Congrats on your booking on '.$_POST['month'] .'/'.$_POST['day'].'/20' .$_POST['year'] . '! We are very excited to show you the wonders of Belize! <br>'
               .$checkedTours
-              . '<br>' . 'You have payed a nonrefundable tour deposit of ' . $tourDeposit ' through Paypal. The remainder of ' .$remainderCost . 'USD or '.$remainderBZE .'Belizian Dollars is to be payed to your tour guide in cash.'
+              . '<br>' . 'You have payed a nonrefundable tour deposit of ' . $tourDeposit . ' through Paypal. The remainder of ' .$remainderCost . 'USD or '.$remainderBZE .'Belizian Dollars is to be payed to your tour guide in cash.'
               . '</div>';
 
   $mail->Body = $messageBody;
