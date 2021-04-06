cbe9986e84dbd883d7cf9e684bd2733aa13e8421
ian.feekes <ian.feekes@baesystems.com>
2021-04-06 16:10:31 -0700

    Fixed many navbar issues. Now the dropdowns seem to work ok and the transitions are working as well. Now mostly style issues to fix, and some cleaning up.

--- assets/js/navbarScript.js
+++ assets/js/navbarScript.js
@@ -1,8 +1,35 @@
-console.log(document.getElementById("dropdownArea"));
+function getHeight(dropdownParam) {
+    let initialHeight = 0; 
+    initialHeight = 100;
+    let el = document.getElementById(dropdownParam);
+    let numChildren = el.getElementsByTagName('a').length; 
+    initialHeight = numChildren * 20; 
+    el.style.height = initialHeight; 
+    console.log(el); 
+    console.log(initialHeight); 
+
+}
+
 
 document.getElementById("dropdownArea").onclick = function () {
-    document.getElementById("dropdownArea").classList.toggle("hamburger");
+    if (event.target == document.getElementById("allTourBtn")) {
+        //event.target.classList.toggle("show");
+        document.getElementById("idc").classList.toggle("show");
+    }
+    else if (event.target == document.getElementById("cruiseTourBtn")) {
+        document.getElementById("cruiseTourDropdown").classList.toggle("show"); 
+    }
+    else {
+        if (document.getElementById("idc").classList.contains("show")) {
+            document.getElementById("idc").classList.toggle("show"); 
+        }
+        if (document.getElementById("cruiseTourDropdown").classList.contains("show")) {
+            document.getElementById("cruiseTourDropdown").classList.toggle("show"); 
+        }
+        document.getElementById("dropdownArea").classList.toggle("hamburger");
+    }
 
 }
 
 //document.getElementById("dropdownArea").classList.toggle("show");
+//getHeight("idc"); 
