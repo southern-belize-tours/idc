f22b529b47e8578a53e01a624fd1e91dc7bc54d9
ian.feekes <ian.feekes@baesystems.com>
2021-04-08 09:09:07 -0700

    Dropdown area and hamburger icons are looking good. Almost ready for full navbar send.

--- assets/js/navbarScript.js
+++ assets/js/navbarScript.js
@@ -12,24 +12,38 @@ function getHeight(dropdownParam) {
 
 
 document.getElementById("dropdownArea").onclick = function () {
-    if (event.target == document.getElementById("allTourBtn")) {
+    console.log(event.target); 
+    if (event.target == document.getElementById("allTourBtn") ||
+        event.target == document.getElementById("landToursText") ||
+        event.target == document.getElementById("landBurger"))
+    {
         //event.target.classList.toggle("show");
         document.getElementById("idc").classList.toggle("show");
+        document.getElementById("landBurger").classList.toggle("hamburger"); 
     }
-    else if (event.target == document.getElementById("cruiseTourBtn")) {
+    else if (event.target == document.getElementById("cruiseTourBtn")   ||
+             event.target == document.getElementById("cruiseToursText") || 
+             event.target == document.getElementById("cruiseBurger")) {
+        console.log("clicked it");
         document.getElementById("cruiseTourDropdown").classList.toggle("show"); 
+        document.getElementById("cruiseBurger").classList.toggle("hamburger"); 
     }
     else {
         if (document.getElementById("idc").classList.contains("show")) {
             document.getElementById("idc").classList.toggle("show"); 
+           //ocument.getElementById("
         }
         if (document.getElementById("cruiseTourDropdown").classList.contains("show")) {
             document.getElementById("cruiseTourDropdown").classList.toggle("show"); 
+            document.getElementById("cruiseBurger").classList.toggle("hamburger"); 
         }
         document.getElementById("dropdownArea").classList.toggle("hamburger");
     }
 
 }
+
+let divs = document.getElementById("dropdownContents").getElementsByTagName("div");
+divs[divs.length - 1].classList.add("rounded"); 
 
 //document.getElementById("dropdownArea").classList.toggle("show");
 //getHeight("idc"); 
